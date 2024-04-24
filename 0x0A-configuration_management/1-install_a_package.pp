ae6fb20efc4ee0b15d5365cfc70a8241d67e55c2
Ahmed-A-T <144383570+Ahmed-A-T@users.noreply.github.com>
2024-04-24 12:04:56 +0200

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
-# installing a package using puppet
-   exec { 'install python packages':
-    command   => 'pip3 install flask flask_restful apiai; touch /root/installed.txt',
-    path      => ['/usr/bin/'],
-    before    => Exec['create custom facter'],
-  }
+#!/usr/bin/pup
+# Install an especific version of flask (2.1.0)
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
+}
